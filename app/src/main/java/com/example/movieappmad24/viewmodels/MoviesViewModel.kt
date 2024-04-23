5d36a12b71e2e85d433b7dcd8ba02ca127cb2031
Elena Corbeanu <113891038+mimimixer@users.noreply.github.com>
2024-04-23 19:34:17 +0200

    Initialize exercise-05-lecture

---
+++ app/src/main/java/com/example/movieappmad24/viewmodels/MoviesViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.movieappmad24.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movieappmad24.data.MovieRepository
+import com.example.movieappmad24.models.Movie
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+
+// Inherit from ViewModel class
+class MoviesViewModel(
+    private val repository: MovieRepository
+) : ViewModel() {
+    private val _movies = MutableStateFlow(listOf<Movie>())
+    val movies: StateFlow<List<Movie>> = _movies.asStateFlow()
+
+
+    init {
+        viewModelScope.launch {
+            repository.getAllMovies().distinctUntilChanged()
+                .collect{ listOfMovies ->
+                    _movies.value = listOfMovies
+                }
+        }
+    }
+
+}
