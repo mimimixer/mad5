766175ff7b9add7ee01b9d298da49c069c61c190
mimimixer <113891038+mimimixer@users.noreply.github.com>
2024-04-23 19:54:25 +0200

    hoff es geht

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -13,12 +13,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             MovieAppMAD24Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Card {
-                        Image(painter = painterResource(id = R.drawable.movie_image), contentDescription = "placeholder_image")
+                class MainActivity : ComponentActivity() {
+                    override fun onCreate(savedInstanceState: Bundle?) {
+                        super.onCreate(savedInstanceState)
+                        setContent {
+                            MovieAppMAD24Theme {
+                                Navigation()
+                            }
+                        }
                     }
                 }
             }
