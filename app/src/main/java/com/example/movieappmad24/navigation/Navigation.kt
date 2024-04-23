11dc3a25ebc5990a5a3554262de0b12255acbb2d
Elena Corbeanu <113891038+mimimixer@users.noreply.github.com>
2024-04-23 19:34:15 +0200

    Initialize exercise-03-solution

---
+++ app/src/main/java/com/example/movieappmad24/navigation/Navigation.kt
@@ -0,0 +1,35 @@
+package com.example.movieappmad24.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.movieappmad24.screens.DetailScreen
+import com.example.movieappmad24.screens.HomeScreen
+import com.example.movieappmad24.screens.WatchlistScreen
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController() // create a NavController instance
+
+    NavHost(navController = navController, // pass the NavController to NavHost
+        startDestination = Screen.HomeScreen.route) {  // pass a start destination
+        composable(route = Screen.HomeScreen.route){   // route with name "homescreen" navigates to HomeScreen composable
+            HomeScreen(navController = navController)
+        }
+
+        composable(
+            route = Screen.DetailScreen.route,
+            arguments = listOf(navArgument(name = DETAIL_ARGUMENT_KEY) {type = NavType.StringType})
+        ) { backStackEntry ->
+            DetailScreen(navController = navController, movieId = backStackEntry.arguments?.getString(
+                DETAIL_ARGUMENT_KEY))
+        }
+
+        composable(route = Screen.WatchlistScreen.route){
+            WatchlistScreen(navController = navController)
+        }
+    }
+}
